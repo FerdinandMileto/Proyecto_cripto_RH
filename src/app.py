91ba75aff04928923222331137f9508d9fa3a38a
rodrigo <rodrigofolatre55@gmail.com>
2025-09-28 17:26:20 -0300

    cracion de los df

--- src/app.py
+++ src/app.py
@@ -1,4 +1,24 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,root_mean_squared_error
 
-# your code here
+
+#primero vamos a armar el modelo y todas las funciones y luego la pagina
+
+
+
+
+
+
+
+
+
+#ahora la pagina
+
+
